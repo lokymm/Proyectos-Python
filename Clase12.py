e686156fe9dd118fd288fb445d223025b3aa00b0
lokymm <lokymm@yahoo.com.ar>
2021-02-28 20:03:19 -0300

    Clase 12 Terminada

---
+++ Clase12.py
@@ -0,0 +1,26 @@
+
+print("###################################################")
+print("Convertidor de Números a Letras")
+print("###################################################")
+
+print("BIENVENIDO !!!!")
+
+numero = int(input("Que número deseas convertir: ?  "))
+
+if numero==1:
+    print("El número es UNO ")
+elif numero==2:
+    print("El número es DOS ")
+elif numero==3:
+    print("El número es TRES ")
+elif numero==4:
+    print("El número es CUATRO ")
+elif numero==5:
+    print("El número es CINCO ")
+
+
+else:
+
+    print ("Solo se puede convertir hasta el número CINCO")
+print ("                                     ")
+print("Fin")
