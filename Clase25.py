d8a7f1964be156f6b784031f4163d50534c0af3b
lokymm <lokymm@yahoo.com.ar>
2021-06-23 20:15:07 -0300

    Clase 25 finalizada

---
+++ Clase25.py
@@ -0,0 +1,9 @@
+print("***************")
+print("* Uso del LEN *")
+print("***************\n")
+
+palabra = input("Ingresa una palabra: ")
+
+longuitud=len(palabra)
+
+print("La cantidad de elementos de la palabra que escribiste es :",longuitud,"\n")
